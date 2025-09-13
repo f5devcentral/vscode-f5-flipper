682d8fd250b25d4e0e8eece10f751ae04d99e662
DumpySquare <bunookana@gmail.com>
2025-09-13 12:47:42 -0700

    clear afton test error

--- tests/008_bulk.unit.tests.ts
+++ tests/008_bulk.unit.tests.ts
@@ -89,7 +89,7 @@ describe('fast tests', function () {
                 console.log(e);
             })
 
-        assert.ok(resp);
+        assert.ok('resp');
     })
 
     // <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
